fd733c73428843d30ce77272c15a708e6179b699
sidnair01 <srnair@ucsd.edu>
2022-02-04 14:14:20 -0800

    fixed substring issue

--- myjavafile.java
+++ myjavafile.java
@@ -1,11 +1,11 @@
 public class myjavafile{
 
     public static String PrintFirstChar(String str){
-        return str.substring(1,1); //creates first error
+        return str.substring(0,1); //creates first error
     }
 
     public static String PrintLastChar(String str){
-        return str.substring(str.length()-1,str.length()-1); //creates second error
+        return str.substring(str.length()-1,str.length()); //creates second error
     }
 
 }
