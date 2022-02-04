3b4c356f85df8bb7968ba460c2e87d050acbc40d
sidnair01 <srnair@ucsd.edu>
2022-02-04 14:15:07 -0800

    Update myjavafile.java

--- myjavafile.java
+++ myjavafile.java
@@ -1,11 +1,11 @@
 public class myjavafile{
 
     public static String PrintFirstChar(String str){
-        return str.substring(0,1); //creates first error
+        return str.substring(0,1); //now fixed
     }
 
     public static String PrintLastChar(String str){
-        return str.substring(str.length()-1,str.length()); //creates second error
+        return str.substring(str.length()-1,str.length()); //now fixed
     }
 
 }
